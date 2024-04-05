22c3419203cb3caa7302f5db63f926659b7f8296
shettydevesh <deveshshetty66@gmail.com>
2024-04-05 15:59:58 +0530

    initial commit

--- sheets.py
+++ sheets.py
@@ -1,9 +1,18 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
 
 # Define the scope and authorize the client
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+credentials_json = os.getenv("GOOGLE_CREDENTIALS_JSON")
+
+# Convert the JSON string to a dictionary
+creds = json.loads(credentials_json)
+creds = Credentials.from_service_account_info(creds, scopes=scopes)
 client = gspread.authorize(creds)
 
 # Open the Google Sheet by ID
