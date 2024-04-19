cf8ed94e6c71688b1768cb034b5d8c2f6680495e
shettydevesh <deveshshetty66@gmail.com>
2024-04-19 19:05:25 +0530

    slight changes in the request

--- gpt.py
+++ gpt.py
@@ -4,7 +4,7 @@ def gpt_answer(query):
     try:
         response = r.post("https://level-ai-personal-coach.level.game/api/coach/prompt", json={
         "question": query,
-        "user_id": "510a7e73-b6b9-11ee-9b59-06099339ad11"
+        "user_id": "004910b3-b455-11ee-9b59-06099339ad11"
         }, headers={"Content-Type": "application/json"})
         answer = response.json()
         return answer['message']
